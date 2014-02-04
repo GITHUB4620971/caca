1b32513daa22b60b790005f4814b6495165df8ce
Víctor <lacuentadevictor@gmail.com>
2014-02-04 19:29:08 +0100

    Funciones sencillas

--- cacaa/index.php
+++ cacaa/index.php
@@ -1,18 +1,28 @@
 <?php
 
+operacion_aritmetica_simple();
 
+
+function operacion_aritmetica_simple() {
+
+//Operación aritmética simple
+    echo "Introduce el número A";
+    $A = leer_linea();
+    echo "Introduce el número B";
+    $B = leer_linea();
+
+    $resultado = $A + $B;
+
+    echo "El resultado es $resultado";
+}
+
+function leer_y_escribir_una_linea() {
 //Leer
-$linea = leer_linea();
+    $linea = leer_linea();
 
-
-
-
-
-
-
-
-//Escribir
-echo $linea;
+    //Escribir
+    echo $linea;
+}
 
 function leer_linea() {
     return trim(fgets(STDIN));              // return the text entered
